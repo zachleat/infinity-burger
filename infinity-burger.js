6a5d22b93daeb41c24f35ba1ed283070b07a174c
Zach Leatherman <zachleatherman@gmail.com>
2019-07-09 17:27:44 -0500

    v2.0.4

--- infinity-burger.js
+++ infinity-burger.js
@@ -23,7 +23,6 @@
 		var x = getRand( -1 * docWidth + 16, 16 );
 		var y = getRand( -40, 40 );
 		var rotate = getRand( -180, 180 );
-		var scale = Math.random() * 2;
 		var transform = 'translateX(' + x + 'px) translateY(' + y + 'px) rotate(' + rotate + 'deg)';
 		bar.style.transform = transform;
 	}
@@ -32,65 +31,71 @@
 	var docHeight;
 	var docWidth;
 	var icon = doc.getElementById( 'infinity-burger' );
-	icon.classList.add( 'enabled' );
+	if( icon ) {
+		icon.classList.add( 'enabled' );
 
-	function addLayer() {
-		if( !docHeight ) {
-			docHeight = getDocHeight();
-		}
-		var node = doc.createElement( 'div' );
-		node.className = 'beforeanimate';
-		icon.appendChild( node );
-		icon.offsetWidth;
-		node.className = '';
-
-		if( !!state ) {
-			if( state === 2 ) {
-				moveRandom( node );
+		function addLayer() {
+			if( !docHeight ) {
+				docHeight = getDocHeight();
 			}
-			if( icon.offsetHeight < docHeight - 20 ) {
+			var node = doc.createElement( 'div' );
+			node.className = 'beforeanimate';
+			icon.appendChild( node );
+			icon.offsetWidth;
+			node.className = '';
+
+			if( !!state ) {
+				if( state === 2 ) {
+					moveRandom( node );
+				}
+				if( icon.offsetHeight < docHeight - 20 ) {
+					requestAnimationFrame( addLayer );
+				} // else trigger complete
+			}
+		}
+
+		icon.addEventListener( 'click', function() {
+			var bars;
+			var j;
+			var k;
+
+			// reset to default state (just 3 bars)
+			if( state > 2 ) {
+				state = 0;
+
+				requestAnimationFrame(function() {
+					icon.innerHTML = '<div></div><div></div><div></div>';
+				});
+			} else if( state === 2 ) {
+				// reset to no transform (vertical infinity hamburger)
+				state++;
+				bars = icon.childNodes;
+				for( j = 0, k = bars.length; j < k; j++ ) {
+					bars[ j ].style.transform = 'none';
+				}
+			} else if( state === 1 ) {
+				// add randomizer animation
+				state++;
+				bars = icon.childNodes;
+
+				if( !docWidth ) {
+					docWidth = getDocWidth();
+				}
+
+				for( j = 0, k = bars.length; j < k; j++ ) {
+					(function() {
+						var bar = bars[ j ];
+						requestAnimationFrame(function() {
+							moveRandom( bar );
+						});
+					})();
+				}
+			} else {
+				// keep adding hamburger layers
+				state = 1;
+				icon.innerHTML = '';
 				requestAnimationFrame( addLayer );
-			} // else trigger complete
-		}
+			}
+		}, false );
 	}
-
-	icon.addEventListener( 'click', function() {
-		// reset to default state (just 3 bars)
-		if( state > 2 ) {
-			state = 0;
-
-			requestAnimationFrame(function() {
-				icon.innerHTML = '<div></div><div></div><div></div>';
-			});
-		} else if( state === 2 ) {
-			// reset to no transform (vertical infinity hamburger)
-			state++;
-			var bars = icon.childNodes;
-			for( var j = 0, k = bars.length; j < k; j++ ) {
-				bars[ j ].style.transform = 'none';
-			}
-		} else if( state === 1 ) {
-			// add randomizer animation
-			state++;
-			var bars = icon.childNodes;
-
-			if( !docWidth ) {
-				docWidth = getDocWidth();
-			}
-
-			for( var j = 0, k = bars.length; j < k; j++ ) {
-				(function() {
-					var bar = bars[ j ];
-					requestAnimationFrame(function() {
-						moveRandom( bar );
-					});
-				})();
-			}
-		} else {
-			// keep adding hamburger layers
-			state = 1;
-			icon.innerHTML = '';
-			requestAnimationFrame( addLayer );
-		}
-	}, false );
 })( typeof global !== "undefined" ? global : this );
